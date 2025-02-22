ca35b708a9447834c61d9161acc7e174c4bda469
takuya-fukumura <blackchoco1114@gmail.com>
2025-02-22 12:22:24 +0900

    コードフォーマッティング

--- src/main/java/com/example/calculator/dto/OldCalculatedData.java
+++ src/main/java/com/example/calculator/dto/OldCalculatedData.java
@@ -1,38 +1,46 @@
 package com.example.calculator.dto;
 
 public class OldCalculatedData {
-	private String oldResult = "";
-	private String oldOperator = "";
-	private boolean errorFlag = true;
+    private String oldResult = "";
+    private String oldOperator = "";
+    private boolean errorFlag = true;
 
-	public OldCalculatedData() {
+    public OldCalculatedData() {
 
-	}
-	public OldCalculatedData(String oldResult,String oldOperator) {
-		this.setOldResult(oldResult);
-		this.setOldOperator(oldOperator);
-	}
-	public OldCalculatedData(String oldResult,String oldOperator,boolean errorFlag) {
-		this.setOldResult(oldResult);
-		this.setOldOperator(oldOperator);
-		this.setErrorFlag(errorFlag);
-	}
-	public void setErrorFlag(boolean errorFlag) {
-		this.errorFlag = errorFlag;
-	}
-	public boolean isErrorFlag() {
-		return this.errorFlag;
-	}
-	public String getOldResult() {
-		return oldResult;
-	}
-	public void setOldResult(String oldResult) {
-		this.oldResult = oldResult;
-	}
-	public String getOldOperator() {
-		return oldOperator;
-	}
-	public void setOldOperator(String oldOperator) {
-		this.oldOperator = oldOperator;
-	}
+    }
+
+    public OldCalculatedData(String oldResult, String oldOperator) {
+        this.setOldResult(oldResult);
+        this.setOldOperator(oldOperator);
+    }
+
+    public OldCalculatedData(String oldResult, String oldOperator, boolean errorFlag) {
+        this.setOldResult(oldResult);
+        this.setOldOperator(oldOperator);
+        this.setErrorFlag(errorFlag);
+    }
+
+    public boolean isErrorFlag() {
+        return this.errorFlag;
+    }
+
+    public void setErrorFlag(boolean errorFlag) {
+        this.errorFlag = errorFlag;
+    }
+
+    public String getOldResult() {
+        return oldResult;
+    }
+
+    public void setOldResult(String oldResult) {
+        this.oldResult = oldResult;
+    }
+
+    public String getOldOperator() {
+        return oldOperator;
+    }
+
+    public void setOldOperator(String oldOperator) {
+        this.oldOperator = oldOperator;
+    }
 }
