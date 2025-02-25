8ed7321d7b979cf86d5ca42e7815c7fd5a285d05
takuya-fukumura <blackchoco1114@gmail.com>
2025-02-25 21:08:28 +0900

    rename

--- src/main/java/com/example/calculator/service/InputService.java
+++ src/main/java/com/example/calculator/service/InputService.java
@@ -15,109 +15,109 @@ public class InputService {
     private static final String DOT = ".";
     private static final String MINUS = "-";
 
-    public String inputProcessing(String display, String clickData) {
+    public String inputProcessing(String formula, String input) {
         // 初期値の場合
-        if (Constants.INITIAL_VALUE.equals(display)) {
-            if (DOT.equals(clickData)) {
-                return display + clickData;
+        if (Constants.INITIAL_VALUE.equals(formula)) {
+            if (DOT.equals(input)) {
+                return formula + input;
             }
             //数字かマイナスで上書きする。それ以外は入らない
-            if (MINUS.equals(clickData) || isNumeric(clickData)) {
-                return clickData;
+            if (MINUS.equals(input) || isNumeric(input)) {
+                return input;
             }
         }
 
-        char lastChar = getLastChar(display.toCharArray());
+        char lastChar = getLastChar(formula.toCharArray());
         if (!isNumeric(lastChar)) { //式の最後尾が記号の時
             // 入力が数字の場合
-            if (isNumeric(clickData)) {
-                return display + clickData;
+            if (isNumeric(input)) {
+                return formula + input;
             }
             // 式の最後が×か÷で、入力がマイナスの場合
-            if (isMinus(clickData) && (isMuD(lastChar))) { // symbolIsMinusとかを見直したい。isMinusとか
-                return display + clickData;
+            if (isMinus(input) && (isMuD(lastChar))) { // symbolIsMinusとかを見直したい。isMinusとか
+                return formula + input;
             }
             // 式の最後がマイナスで、入力がマイナスの場合
-            if (isMinus(clickData) && (isMinus(lastChar))) {
-                return CommonUtil.deleteLastChar(display) + "＋"; //プラスマイナス反転させる
+            if (isMinus(input) && (isMinus(lastChar))) {
+                return CommonUtil.deleteLastChar(formula) + "＋"; //プラスマイナス反転させる
             }
             // 式の最後がプラスで、入力がマイナスの場合
-            if (isMinus(clickData) && (isPlus(lastChar))) {
-                return CommonUtil.deleteLastChar(display) + "-"; //プラスマイナス反転させる
+            if (isMinus(input) && (isPlus(lastChar))) {
+                return CommonUtil.deleteLastChar(formula) + "-"; //プラスマイナス反転させる
             }
         }
 
         if (isNumeric(lastChar)) { //最後尾が数字の時は、ほぼなんでも入る
             //最後の塊が数字グループでピリオドが含まれるならなにもしない
-            if (checkPeriod(display) && ".".equals(clickData)) {
+            if (checkPeriod(formula) && ".".equals(input)) {
             } else {
-                return display + clickData;
+                return formula + input;
             }
         }
-        return display;
+        return formula;
     }
 
     /**
      * ピリオドの重複防止
      */
-    public boolean checkPeriod(String display) {
+    public boolean checkPeriod(String formula) {
         //塊に分ける
         //最後の塊を取り出して、ピリオドが含まれているかチェックする
-        if (display == null) {
+        if (formula == null) {
             return false;
         }
-        String[] str = Calculation.splitFormula(display);
+        String[] str = Calculation.splitFormula(formula);
         return isPeriod(str[str.length - 1]);
     }
 
-    public boolean isPeriod(String c) {
-        return c != null && c.contains(".");
+    public boolean isPeriod(String input) {
+        return input != null && input.contains(".");
     }
 
     /**
      * 入力系処理であることを判断する
-     * 数字or四則演算子orピリオド
+     * 数字 or 四則演算子 or ピリオド
      */
-    public boolean checkInput(String c) {
-        return isNumeric(c) || Pattern.matches("^\\.$", c) ||
-                isMinus(c) || Pattern.matches("^＋$", c) || Pattern.matches("^×$|^÷$", c);
+    public boolean checkInput(String input) {
+        return isNumeric(input) || Pattern.matches("^\\.$", input) ||
+                isMinus(input) || Pattern.matches("^＋$", input) || Pattern.matches("^×$|^÷$", input);
     }
 
     /**
      * 記号ではないことを確認するメソッド
      * （数字であることを確認する）
      */
-    public boolean isNumeric(char c) {
-        return Pattern.matches("^\\d*$", String.valueOf(c));
+    public boolean isNumeric(char input) {
+        return Pattern.matches("^\\d*$", String.valueOf(input));
     }
 
-    public boolean isNumeric(String c) {
-        return Pattern.matches("^\\d*$", c);
+    public boolean isNumeric(String input) {
+        return Pattern.matches("^\\d*$", input);
     }
 
     /**
      * 記号がマイナスであることを確認する
      */
-    public boolean isMinus(char c) {
-        return Pattern.matches("^-$", String.valueOf(c));
+    public boolean isMinus(char input) {
+        return Pattern.matches("^-$", String.valueOf(input));
     }
 
-    public boolean isMinus(String c) {
-        return Pattern.matches("^-$", c);
+    public boolean isMinus(String input) {
+        return Pattern.matches("^-$", input);
     }
 
     /**
      * 記号がプラスであることを確認する
      */
-    public boolean isPlus(char c) {
-        return Pattern.matches("^＋$", String.valueOf(c));
+    public boolean isPlus(char input) {
+        return Pattern.matches("^＋$", String.valueOf(input));
     }
 
     /**
      * 記号が×,÷であることを確認する
      */
-    public boolean isMuD(char c) { //引数Stringのほうがいいのでは？オーバーロードする？
-        return Pattern.matches("^×$|^÷$", String.valueOf(c));
+    public boolean isMuD(char input) { //引数Stringのほうがいいのでは？オーバーロードする？
+        return Pattern.matches("^×$|^÷$", String.valueOf(input));
     }
 
     /**
